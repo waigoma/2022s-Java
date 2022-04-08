9d254fab963b9e0b0dc77d98bb0b8ebb01f3c7f9
waigoma <waichi0302@gmail.com>
2022-04-08 12:03:41 +0900

    done SumRecursion
    改善点あり

--- src/main/java/j4/lesson01ex/SumRecursion.java
+++ src/main/java/j4/lesson01ex/SumRecursion.java
@@ -1,4 +1,44 @@
 package j4.lesson01ex;
 
+import javax.swing.*;
+
 public class SumRecursion {
+    public static void main(String[] args) {
+        String inputText = JOptionPane.showInputDialog("整数を入力してください。");
+        int inputNumber = Integer.parseInt(inputText);
+
+        int total = 0;
+        String resultMessage = "";
+        boolean isAbbreviate = false;
+        boolean isFirst = true;
+
+        if (inputNumber % 2 == 0) {
+            int abbrNumber = 4;
+            for (int i = 2; ; i += 2) {
+                if (i > inputNumber) break;
+                if (!isFirst && i <= abbrNumber) resultMessage += " + ";
+
+                total += i;
+                if (i <= abbrNumber) resultMessage += i;
+                else isAbbreviate = true;
+                isFirst = false;
+            }
+        } else {
+            int abbrNumber = 3;
+            for (int i = 1; ; i += 2) {
+                if (i > inputNumber) break;
+                if (!isFirst && i <= abbrNumber) resultMessage += " + ";
+
+                total += i;
+                if (i <= abbrNumber) resultMessage += i;
+                else isAbbreviate = true;
+                isFirst = false;
+            }
+        }
+        if (isAbbreviate) resultMessage += " + ... + " + inputText;
+        resultMessage += " = " + total;
+
+        JOptionPane.showMessageDialog(null, resultMessage);
+        System.out.println(total);
+    }
 }
