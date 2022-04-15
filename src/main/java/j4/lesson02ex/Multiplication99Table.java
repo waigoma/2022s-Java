eab951bf1c4adaee175dcd137ea865023dbaf524
waigoma <waichi0302@gmail.com>
2022-04-15 11:15:50 +0900

    done Multiplication99Table

--- src/main/java/j4/lesson02ex/Multiplication99Table.java
+++ src/main/java/j4/lesson02ex/Multiplication99Table.java
@@ -1,4 +1,29 @@
 package j4.lesson02ex;
 
-public class Multiplication99Table {
+import javax.swing.*;
+import java.awt.*;
+
+public class Multiplication99Table extends JFrame {
+    public Multiplication99Table(String title, int x, int y, int width, int height) {
+        setTitle(title);
+        setLocation(x, y);
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.WHITE);
+    }
+
+    public static void main(String[] args) {
+        Multiplication99Table frame = new Multiplication99Table("Multiplication99Table", 200, 200, 400, 400);
+        frame.setLayout(new GridLayout(9, 9, 5, 5));
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                JLabel label = new JLabel(i * j + "");
+                label.setFont(new Font("Times New Roman", Font.BOLD, 24));
+                frame.add(label);
+            }
+        }
+
+        frame.setVisible(true);
+    }
 }
