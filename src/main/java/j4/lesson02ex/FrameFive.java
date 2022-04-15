f036b1f4cf28ed1f735d5863e6d787c961c0c148
waigoma <waichi0302@gmail.com>
2022-04-15 11:00:38 +0900

    done FrameFive

--- src/main/java/j4/lesson02ex/FrameFive.java
+++ src/main/java/j4/lesson02ex/FrameFive.java
@@ -1,4 +1,33 @@
 package j4.lesson02ex;
 
-public class FrameFive {
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameFive extends JFrame {
+    public FrameFive(String title, int x, int y, int width, int height, Color color) {
+        setTitle(title);
+        setLocation(x, y);
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(color);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        int width = screenWidth / 3;
+        int height = screenHeight / 3;
+
+        new FrameFive("Frame1", 0, 0, width, height, Color.YELLOW);
+        new FrameFive("Frame2", width * 2, 0, width, height, Color.GREEN);
+
+        new FrameFive("Frame3", width, height, width, height, Color.RED);
+
+        new FrameFive("Frame4", 0, height * 2, width, height, Color.BLUE);
+        new FrameFive("Frame5", width * 2, height * 2, width, height, Color.BLACK);
+    }
 }
