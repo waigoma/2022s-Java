aa4027c5dafb7353912cb747630abe7e88a36369
waigoma <waichi0302@gmail.com>
2022-04-19 21:56:51 +0900

    done ImageDisplay

--- src/main/java/j4/lesson02ex/ImageDisplay.java
+++ src/main/java/j4/lesson02ex/ImageDisplay.java
@@ -27,47 +27,41 @@ public class ImageDisplay extends JFrame {
         ));
 
         String inputStr = JOptionPane.showInputDialog("Center Color: r, g, b, y or bk?");
+        int inputValue = 0;
 
         switch (inputStr) {
             case "r":
-                frame.add(new JLabel(images.get(0)), BorderLayout.CENTER);
-                frame.add(new JLabel(images.get(1)), BorderLayout.EAST);
-                frame.add(new JLabel(images.get(2)), BorderLayout.SOUTH);
-                frame.add(new JLabel(images.get(3)), BorderLayout.WEST);
-                frame.add(new JLabel(images.get(4)), BorderLayout.NORTH);
+                inputValue = 0;
                 break;
             case "g":
-                frame.add(new JLabel(images.get(1)), BorderLayout.CENTER);
-                frame.add(new JLabel(images.get(2)), BorderLayout.EAST);
-                frame.add(new JLabel(images.get(3)), BorderLayout.SOUTH);
-                frame.add(new JLabel(images.get(4)), BorderLayout.WEST);
-                frame.add(new JLabel(images.get(0)), BorderLayout.NORTH);
+                inputValue = 1;
                 break;
             case "b":
-                frame.add(new JLabel(images.get(2)), BorderLayout.CENTER);
-                frame.add(new JLabel(images.get(3)), BorderLayout.EAST);
-                frame.add(new JLabel(images.get(4)), BorderLayout.SOUTH);
-                frame.add(new JLabel(images.get(0)), BorderLayout.WEST);
-                frame.add(new JLabel(images.get(1)), BorderLayout.NORTH);
+                inputValue = 2;
                 break;
             case "y":
-                frame.add(new JLabel(images.get(3)), BorderLayout.CENTER);
-                frame.add(new JLabel(images.get(4)), BorderLayout.EAST);
-                frame.add(new JLabel(images.get(0)), BorderLayout.SOUTH);
-                frame.add(new JLabel(images.get(1)), BorderLayout.WEST);
-                frame.add(new JLabel(images.get(2)), BorderLayout.NORTH);
+                inputValue = 3;
                 break;
             case "bk":
-                frame.add(new JLabel(images.get(4)), BorderLayout.CENTER);
-                frame.add(new JLabel(images.get(0)), BorderLayout.EAST);
-                frame.add(new JLabel(images.get(1)), BorderLayout.SOUTH);
-                frame.add(new JLabel(images.get(2)), BorderLayout.WEST);
-                frame.add(new JLabel(images.get(3)), BorderLayout.NORTH);
+                inputValue = 4;
                 break;
             default:
                 JOptionPane.showMessageDialog(null, inputStr + " is wrong input!\nPlease enter r, g, b, y or bk.\nRe-execute the program and input again.");
                 System.exit(0);
         }
+
+        for (int i = 0; i < images.size() - inputValue; i++) {
+            ImageIcon tmp = images.get(i);
+            images.remove(i);
+            images.add(tmp);
+        }
+
+        frame.add(new JLabel(images.get(0)), BorderLayout.CENTER);
+        frame.add(new JLabel(images.get(1)), BorderLayout.EAST);
+        frame.add(new JLabel(images.get(2)), BorderLayout.SOUTH);
+        frame.add(new JLabel(images.get(3)), BorderLayout.WEST);
+        frame.add(new JLabel(images.get(4)), BorderLayout.NORTH);
+
         frame.setVisible(true);
     }
 }
