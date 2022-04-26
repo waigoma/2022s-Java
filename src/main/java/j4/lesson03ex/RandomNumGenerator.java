c3ae385892fcc57b365c9a1b7c98ee7e6eb07cf7
waigoma <waichi0302@gmail.com>
2022-04-27 02:34:07 +0900

    done RandomNumGenerator

--- src/main/java/j4/lesson03ex/RandomNumGenerator.java
+++ src/main/java/j4/lesson03ex/RandomNumGenerator.java
@@ -3,11 +3,15 @@ package j4.lesson03ex;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.*;
 
 public class RandomNumGenerator extends JPanel implements ActionListener {
-    public static void main(String[] args){
+    private boolean isUniform = false;
+    private boolean isNormal = false;
+
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,18 +47,46 @@ public class RandomNumGenerator extends JPanel implements ActionListener {
     }
 
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.blue);
         g.setFont(new Font("Century", Font.PLAIN, 24));
         g.drawString("Random Number Generator", 100, 30);
         g.drawRect(90, 50, 300, 300);
         g.setFont(new Font("Century", Font.PLAIN, 18));
-        g.drawString("Display 1000 random numbers in the 300x300 rectangle", 100, 400);
+        if (!isUniform && !isNormal) {
+            g.drawString("Display 1000 random numbers", 100, 120);
+            g.drawString("in the 300x300 rectangle", 120, 150);
+        } else if (isUniform && !isNormal) {
+            g.setColor(Color.RED);
+            for (int i = 0; i < 1000; i++) {
+                int x = (int) (Math.random() * 280);
+                int y = (int) (Math.random() * 280);
+                g.fillOval(98 + x, 58 + y, 5, 5);
+            }
+        } else if (!isUniform && isNormal) {
+            g.setColor(Color.RED);
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++) {
+                int x = (int) (r.nextGaussian() * 40 + 150);
+                int y = (int) (r.nextGaussian() * 40 + 150);
+                g.fillOval(98 + x, 58 + y, 5, 5);
+            }
+        }
     }
 
     @Override
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Uniform Distribution":
+                isUniform = true;
+                isNormal = false;
+                break;
+            case "Normal Distribution":
+                isUniform = false;
+                isNormal = true;
+                break;
+        }
         repaint();
     }
 }
