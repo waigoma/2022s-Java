883cb204758f4823b4fa10d43554a6d48f138acd
waigoma <waichi0302@gmail.com>
2022-04-27 17:12:45 +0900

    update MouseDrawShapeColor

--- src/main/java/j4/lesson03ex/MouseDrawShapeColor.java
+++ src/main/java/j4/lesson03ex/MouseDrawShapeColor.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class MouseDrawShapeColor extends JFrame implements MouseListener, ActionListener {
