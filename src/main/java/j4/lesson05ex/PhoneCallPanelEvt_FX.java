171058178e78d9b51b09438433375824322b0d8f
waigoma <waichi0302@gmail.com>
2022-05-13 13:27:54 +0900

    update

--- src/main/java/j4/lesson05ex/PhoneCallPanelEvt_FX.java
+++ src/main/java/j4/lesson05ex/PhoneCallPanelEvt_FX.java
@@ -1,4 +1,35 @@
 package j4.lesson05ex;
 
-public class PhoneCallPanelEvt_FX {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class PhoneCallPanelEvt_FX extends Application {
+    private Label label;
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 260, 300);
+
+        label = new Label("");
+
+        GridPane grid = new GridPane();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private class PhoneButton implements EventHandler<MouseEvent> {
+        @Override
+        public void handle(MouseEvent e) {
+            System.out.println(e.getEventType());
+        }
+    }
 }
