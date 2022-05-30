4e3cd9248aa9731dfab5b14aac95f4d252d58477
waigoma <waichi0302@gmail.com>
2022-05-31 01:27:14 +0900

    done Register_FX

--- src/main/java/j4/lesson07ex/Register_FX.java
+++ src/main/java/j4/lesson07ex/Register_FX.java
@@ -1,6 +1,7 @@
 package j4.lesson07ex;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -27,49 +28,21 @@ public class Register_FX extends Application {
         Scene scene = new Scene(root, 650, 400);
 
         // 左側 vbox
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setStyle("-fx-background-color: #336699;");
-
-        Label label = new Label("Registration List");
-
-        HBox name = createLabelField("Name: ", new TextField());
-        HBox password = createLabelField("Password: ", new PasswordField());
-        HBox confirm = createLabelField("Confirm: ", new PasswordField());
-        HBox email = createLabelField("Email: ", new TextField());
-
-        ToggleGroup genderGroup = new ToggleGroup();
-        RadioButton maleRadioButton = new RadioButton("Male");
-        maleRadioButton.setToggleGroup(genderGroup);
-        RadioButton femaleRadioButton = new RadioButton("Female");
-        femaleRadioButton.setToggleGroup(genderGroup);
-        HBox gender = createLabelField("", maleRadioButton, femaleRadioButton);
-
-        initComboBox(yearComboBox, 1990, 2009);
-        initComboBox(monthComboBox, 1, 12);
-        initComboBox(dayComboBox, 1, 31);
-        HBox birthday = createLabelField("Birthday: ", yearComboBox, monthComboBox, dayComboBox);
-
-        CheckBox sports = new CheckBox("Sports");
-        CheckBox music = new CheckBox("Music");
-        CheckBox game = new CheckBox("Game");
-        HBox hobby = createLabelField("Hobby: ", sports, music, game);
-
-        TextArea textArea = new TextArea();
-        textArea.setPrefSize(220, 100);
-        HBox textAreaBox = createLabelField("Biography: ", false, textArea);
-
-        Button confirmButton = new Button("Confirm");
-        Button clearButton = new Button("Clear");
-        HBox buttonBox = createLabelField("", confirmButton, clearButton);
-
-        vbox.getChildren().addAll(label, name, password, confirm, email, gender, birthday, hobby, textAreaBox, buttonBox);
+        VBox leftVBox = leftVBox();
 
         // 右側 vbox
         VBox vbox2 = new VBox();
         vbox2.setAlignment(Pos.CENTER);
         Label rightTopLabel = new Label("Registration Result");
-        rightTopLabel.setFont(Font.font("Century", FontWeight.BOLD, 24));
+        rightTopLabel.setFont(Font.font("Century", FontWeight.BOLD, 18));
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(220d, 290d);
+        textArea.setEditable(false);
+
+        vbox2.getChildren().addAll(rightTopLabel, textArea);
+        vbox2.setPadding(new Insets(0, 15, 15, 10));
+        vbox2.setSpacing(10);
 
         // 上のラベル
         Label topLabel = new Label("Register & Display");
@@ -78,12 +51,71 @@ public class Register_FX extends Application {
 
         // 最後のレイアウト
         root.setTop(hbox);
-        root.setLeft(vbox);
+        root.setLeft(leftVBox);
         root.setCenter(vbox2);
 
         stage.setScene(scene);
         stage.setTitle("Register_FXOption");
         stage.show();
+    }
+
+    private VBox leftVBox() {
+        Label label = new Label("Registration List");
+        label.setFont(Font.font("Century", FontWeight.BOLD, 18));
+
+        HBox name = createLabelField("Name: ", new TextField());
+        name.setAlignment(Pos.CENTER_RIGHT);
+        name.setPadding(new Insets(0, 10, 0, 0));
+        HBox password = createLabelField("Password: ", new PasswordField());
+        password.setAlignment(Pos.CENTER_RIGHT);
+        password.setPadding(new Insets(0, 10, 0, 0));
+        HBox confirm = createLabelField("Confirm: ", new PasswordField());
+        confirm.setAlignment(Pos.CENTER_RIGHT);
+        confirm.setPadding(new Insets(0, 10, 0, 0));
+        HBox email = createLabelField("Email: ", new TextField());
+        email.setAlignment(Pos.CENTER_RIGHT);
+        email.setPadding(new Insets(0, 10, 0, 0));
+
+        ToggleGroup genderGroup = new ToggleGroup();
+        RadioButton maleRadioButton = new RadioButton("Male");
+        maleRadioButton.setToggleGroup(genderGroup);
+        RadioButton femaleRadioButton = new RadioButton("Female");
+        femaleRadioButton.setToggleGroup(genderGroup);
+        HBox gender = createLabelField("", maleRadioButton, femaleRadioButton);
+        gender.setSpacing(40);
+        gender.setPadding(new Insets(0, 0, 0, 10));
+
+        initComboBox(yearComboBox, 1990, 2009);
+        initComboBox(monthComboBox, 1, 12);
+        initComboBox(dayComboBox, 1, 31);
+        HBox birthday = createLabelField("Birthday: ", yearComboBox, monthComboBox, dayComboBox);
+        birthday.setSpacing(5);
+
+        CheckBox sports = new CheckBox("Sports");
+        CheckBox music = new CheckBox("Music");
+        CheckBox game = new CheckBox("Game");
+        HBox hobby = createLabelField("Hobby: ", sports, music, game);
+        hobby.setSpacing(5);
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(220, 100);
+        HBox textAreaBox = createLabelField("Biography: ", false, textArea);
+        textAreaBox.setPadding(new Insets(0, 5, 0, 10));
+
+        Button confirmButton = new Button("Confirm");
+        Button clearButton = new Button("Clear");
+        HBox buttonBox = createLabelField("", confirmButton, clearButton);
+        buttonBox.setSpacing(40);
+        buttonBox.setPadding(new Insets(0, 0, 0, 10));
+
+        // レイアウト
+        VBox vbox = new VBox();
+//        vbox.setStyle("-fx-background-color: #336699;");
+        vbox.getChildren().addAll(label, name, password, confirm, email, gender, birthday, hobby, textAreaBox, buttonBox);
+        vbox.setSpacing(5);
+        vbox.setAlignment(Pos.CENTER);
+
+        return vbox;
     }
 
     private void initComboBox(ComboBox<Integer> cb, int startNum, int endNum) {
@@ -94,14 +126,18 @@ public class Register_FX extends Application {
     private HBox createLabelField(String labelText, boolean isSetSize, Node... nodes) {
         HBox hbox = new HBox();
         hbox.setAlignment(Pos.CENTER);
+
         if (isSetSize) hbox.setPrefWidth(220);
-//        hbox.setMaxSize(1000, 1000);
+
+        hbox.setMaxSize(1000, 1000);
         Label label = new Label(labelText);
         label.setFont(Font.font("Century", 12));
         hbox.getChildren().addAll(label);
 
-        for (Node node : nodes)
+        for (Node node : nodes) {
+            if (node instanceof TextField) ((TextField) node).setPrefWidth(200);
             hbox.getChildren().add(node);
+        }
 
         return hbox;
     }
