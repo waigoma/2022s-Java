63a4a522d96a1f13a5054448665f24ca372168e4
waigoma <waichi0302@gmail.com>
2022-06-01 02:12:01 +0900

    update RegisterConfirm_FX

--- src/main/java/j4/lesson07ex/RegisterConfirm_FX.java
+++ src/main/java/j4/lesson07ex/RegisterConfirm_FX.java
@@ -1,4 +1,159 @@
 package j4.lesson07ex;
 
-public class RegisterConfirm_FX {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class RegisterConfirm_FX extends Application {
+    private final ComboBox<Integer> yearComboBox = new ComboBox<>();
+    private final ComboBox<Integer> monthComboBox = new ComboBox<>();
+    private final ComboBox<Integer> dayComboBox = new ComboBox<>();
+
+    private final TextField nameField = new TextField();
+    private final PasswordField passwordField = new PasswordField();
+    private final PasswordField confirmPasswordField = new PasswordField();
+    private final TextField emailField = new TextField();
+    private final ToggleGroup genderGroup = new ToggleGroup();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 650, 400);
+
+        // 上のラベル
+        Label topLabel = new Label("Register & Display");
+        topLabel.setFont(Font.font("Century", FontWeight.BOLD, 32));
+        HBox hbox = createLabelField("", topLabel);
+
+        // 左側 vbox
+        VBox leftVBox = leftVBox();
+
+        // 右側 vbox
+        VBox rightBox = rightBox();
+
+        // 最後のレイアウト
+        root.setTop(hbox);
+        root.setLeft(leftVBox);
+        root.setCenter(rightBox);
+
+        stage.setScene(scene);
+        stage.setTitle("Register_FXOption");
+        stage.show();
+    }
+
+    private VBox leftVBox() {
+        Label label = new Label("Registration List");
+        label.setFont(Font.font("Century", FontWeight.BOLD, 18));
+
+        HBox name = createLabelField("Name: ", nameField);
+        name.setAlignment(Pos.CENTER_RIGHT);
+        name.setPadding(new Insets(0, 10, 0, 0));
+        HBox password = createLabelField("Password: ", passwordField);
+        password.setAlignment(Pos.CENTER_RIGHT);
+        password.setPadding(new Insets(0, 10, 0, 0));
+        HBox confirm = createLabelField("Confirm: ", confirmPasswordField);
+        confirm.setAlignment(Pos.CENTER_RIGHT);
+        confirm.setPadding(new Insets(0, 10, 0, 0));
+        HBox email = createLabelField("Email: ", emailField);
+        email.setAlignment(Pos.CENTER_RIGHT);
+        email.setPadding(new Insets(0, 10, 0, 0));
+
+        RadioButton maleRadioButton = new RadioButton("Male");
+        maleRadioButton.setToggleGroup(genderGroup);
+        RadioButton femaleRadioButton = new RadioButton("Female");
+        femaleRadioButton.setToggleGroup(genderGroup);
+        HBox gender = createLabelField("", maleRadioButton, femaleRadioButton);
+        gender.setSpacing(40);
+        gender.setPadding(new Insets(0, 0, 0, 10));
+
+        initComboBox(yearComboBox, 1990, 2009);
+        initComboBox(monthComboBox, 1, 12);
+        initComboBox(dayComboBox, 1, 31);
+        HBox birthday = createLabelField("Birthday: ", yearComboBox, monthComboBox, dayComboBox);
+        birthday.setSpacing(5);
+
+        CheckBox sports = new CheckBox("Sports");
+        CheckBox music = new CheckBox("Music");
+        CheckBox game = new CheckBox("Game");
+        HBox hobby = createLabelField("Hobby: ", sports, music, game);
+        hobby.setSpacing(5);
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(220, 100);
+        HBox textAreaBox = createLabelField("Biography: ", false, textArea);
+        textAreaBox.setPadding(new Insets(0, 5, 0, 10));
+
+        Button confirmButton = new Button("Confirm");
+        Button clearButton = new Button("Clear");
+        HBox buttonBox = createLabelField("", confirmButton, clearButton);
+        buttonBox.setSpacing(40);
+        buttonBox.setPadding(new Insets(0, 0, 0, 10));
+
+        // レイアウト
+        VBox vbox = new VBox();
+//        vbox.setStyle("-fx-background-color: #336699;");
+        vbox.getChildren().addAll(label, name, password, confirm, email, gender, birthday, hobby, textAreaBox, buttonBox);
+        vbox.setSpacing(5);
+        vbox.setAlignment(Pos.CENTER);
+
+        return vbox;
+    }
+
+    private VBox rightBox() {
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        Label rightTopLabel = new Label("Registration Result");
+        rightTopLabel.setFont(Font.font("Century", FontWeight.BOLD, 18));
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(220d, 290d);
+        textArea.setEditable(false);
+
+        vbox.getChildren().addAll(rightTopLabel, textArea);
+        vbox.setPadding(new Insets(0, 15, 15, 10));
+        vbox.setSpacing(10);
+
+        return vbox;
+    }
+
+    private void initComboBox(ComboBox<Integer> cb, int startNum, int endNum) {
+        for (int i = startNum; i <= endNum; i++)
+            cb.getItems().add(i);
+    }
+
+    private HBox createLabelField(String labelText, boolean isSetSize, Node... nodes) {
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+
+        if (isSetSize) hbox.setPrefWidth(220);
+
+        hbox.setMaxSize(1000, 1000);
+        Label label = new Label(labelText);
+        label.setFont(Font.font("Century", 12));
+        hbox.getChildren().addAll(label);
+
+        for (Node node : nodes) {
+            if (node instanceof TextField) ((TextField) node).setPrefWidth(200);
+            hbox.getChildren().add(node);
+        }
+
+        return hbox;
+    }
+
+    private HBox createLabelField(String labelText, Node... nodes) {
+        return createLabelField(labelText, true, nodes);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
