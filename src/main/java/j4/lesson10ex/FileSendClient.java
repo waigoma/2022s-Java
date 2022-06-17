7938437b04581d9b8dabd8edac2e830db895d75d
waigoma <waichi0302@gmail.com>
2022-06-17 12:29:58 +0900

    update

--- src/main/java/j4/lesson10ex/FileSendClient.java
+++ src/main/java/j4/lesson10ex/FileSendClient.java
@@ -1,4 +1,35 @@
 package j4.lesson10ex;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class FileSendClient {
+    public static void main(String[] args) throws IOException
+    {
+        // Create a socket and connect to remote server
+        //Socket socket = new Socket(addr, EchoServer.PORT);
+        //InetAddress addr = InetAddress.getByName("localhost");
+
+        try (Socket socket = new Socket("localhost", EchoServer.PORT)) {
+            System.out.println("Client is connecting to the Server ---");
+            System.out.println("Type in console; Type END for closing connection");
+            // Get the input stream
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Get the output stream
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+            while (true) {
+                System.out.print("Client=>Server: ");
+                Scanner input = new Scanner(System.in);
+                String line = input.nextLine();
+                out.println(line);
+                String str = in.readLine();
+                System.out.println("Server=>Client: " + str);
+                if (line.equalsIgnoreCase("END")) break;
+            }
+
+            System.out.println("Client is closed ---");
+        }
+    }
 }
