d3d92761b5a6569b30a5bc923cc997cd6b5650b5
waigoma <waichi0302@gmail.com>
2022-06-19 14:38:26 +0900

    done FileSendClient

--- src/main/java/j4/lesson10ex/FileSendClient.java
+++ src/main/java/j4/lesson10ex/FileSendClient.java
@@ -14,20 +14,24 @@ public class FileSendClient {
         try (Socket socket = new Socket("localhost", EchoServer.PORT)) {
             System.out.println("Client is connecting to the Server ---");
             System.out.println("Type in console; Type END for closing connection");
+
             // Get the input stream
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             // Get the output stream
             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-            while (true) {
-                System.out.print("Client=>Server: ");
-                Scanner input = new Scanner(System.in);
-                String line = input.nextLine();
+            System.out.print("Send File: ");
+            Scanner input = new Scanner(System.in);
+            String fileName = input.nextLine();
+
+            Scanner file = new Scanner(new File(fileName));
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
                 out.println(line);
                 String str = in.readLine();
                 System.out.println("Server=>Client: " + str);
-                if (line.equalsIgnoreCase("END")) break;
             }
+            out.println("END");
 
             System.out.println("Client is closed ---");
         }
