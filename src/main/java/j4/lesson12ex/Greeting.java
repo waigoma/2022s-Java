b502b9f4cbba8dc94c9f99ce0b311283c74ae727
waigoma <waichi0302@gmail.com>
2022-07-01 12:01:28 +0900

    done Greeting

--- src/main/java/j4/lesson12ex/Greeting.java
+++ src/main/java/j4/lesson12ex/Greeting.java
@@ -1,4 +1,43 @@
 package j4.lesson12ex;
 
-public class Greeting {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/lesson12ex/Greeting")
+public class Greeting extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        int hour = date.getHours();
+        String time = format.format(date);
+        String greeting = "";
+        String imgSource = "";
+
+        if (5 < hour && hour < 12) {
+            greeting = "Good morning";
+            imgSource = "../lesson12ex/morning.jpg";
+        } else if (12 <= hour && hour < 18) {
+            greeting = "Good afternoon";
+            imgSource = "../lesson12ex/afternoon.jpg";
+        } else {
+            greeting = "Good evening";
+            imgSource = "../lesson12ex/evening.jpg";
+        }
+
+        out.println("<html><head><title>Greeting</title></head>");
+        out.println("<body>");
+        out.println("<img src=\"" + imgSource + "\">");
+        out.println("<p>" + time + "</p>");
+        out.println("<h1>" + greeting + "</h1></body></html>");
+    }
 }
